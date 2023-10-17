4e58b5cc90c20b86df0ac134dd36c60b85534cbd
NigelLu <nigellu@outlook.com>
2023-10-16 22:52:36 -0400

    add: added admin site customizations

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Choice)
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
